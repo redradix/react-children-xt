d853166215b0e3d61a5d22b9eae844b29deb3200
Virgilio Gómez <virgilio@redradix.com>
2023-03-24 13:08:40 +0100

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# react-children-xt
+
+> Count and map over React children flattening fragments and not taking into account elements that won't be rendered such as `undefined` or `null`
+
+## Why to use it
+
+On one hand, accessing directly to `children.length` or `children.map` doesn't works as expected when receiving a single child, as it is not encapsulated in an array. The solution may be to use `React.Children.count` and `React.Children.map` but this may also have an unexpected behaviour when treating as valid children elements that won't be rendered such as `undefined` or `null`.
+
+Moreover, sometimes we need to flatten `Fragment`s and treat its content as direct children of the Component so this package does it by default. It is possible to disable this behaviour passing `false` as the last argument of the methods.
+
+## Installation
+
+```
+npm install --save react-children-xt
+```
+
+or
+
+```
+yarn add react-children-xt
+```
+
+## Usage
+
+```jsx
+import React from 'react'
+import { countChildren, mapChildren } from 'react-children-xt'
+
+function MyParentComponent() {
+  return (
+    <MyComponent>
+      <div>One</div>
+      {undefined}
+      {null}
+      <div>Two</div>
+      <>
+        <div>Three</div>
+        <div>Four</div>
+      </>
+    </MyComponent>
+  )
+}
+
+function MyComponent({ children }) {
+  // Native way to count React children.
+  // It counts the two main `div`s, `undefined`, `null` and the `Fragment`.
+  const badChildrenCount = React.Children.count(children)
+  console.log(badChildrenCount) // output: 5
+
+  // Using `countChildren` method.
+  // It counts the four `div`s, flattening the `Fragment`.
+  const childrenCount = countChildren(children)
+  console.log(childrenCount) // output: 4
+
+  // Using `countChildren` method without flattening the `Fragment`.
+  // It counts the two main `div`s and the `Fragment`.
+  const mainChildrenCount = countChildren(children, false)
+  console.log(mainChildrenCount) // output: 3
+
+  // `MyComponent` will render the four `div`s.
+  return (
+    <>
+      {mapChildren(children, (child, index) => (
+        <React.Fragment key={index}>{child}</React.Fragment>
+      ))}
+    </>
+  )
+}
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+let React
+
+try {
+  React = require('react')
+} catch (err) {
+  throw new Error('This package must be used in a React app')
+}
+
+/**
+ * childrenToArray – Convert react children to an array. Works for a single
+ * child. Omits children that will not be rendered (null, undefined, false).
+ * Flattens children inside fragments
+ * REVIEW we are calling Array.prototype.flat() once in every recursive call
+ * @param {React.Children} children - Children object as provided by react
+ * @param {Boolean} flatten - Whether or not to flatten children inside
+ * fragments. Defaults to true
+ * @return {Array} An array of react elements
+ */
+const childrenToArray = (children, flatten = true) => {
+  const array = React.Children.toArray(children)
+  if (flatten) {
+    return array
+      .map(child =>
+        child.type === React.Fragment
+          ? childrenToArray(child.props.children, flatten)
+          : child,
+      )
+      .flat()
+  }
+  return array
+}
+
+/**
+ * countChildren – Count the number of children. Omits children that will not be
+ * rendered (null, undefined, false). Flattens children inside fragments
+ * @param {React.Children} children - Children object as provided by react
+ * @param {Boolean} flatten - Whether or not to flatten children inside
+ * fragments. Defaults to true
+ * @return {Number} The actual number of children
+ */
+const countChildren = (children, flatten = true) =>
+  React.Children.count(childrenToArray(children, flatten))
+
+/**
+ * mapChildren – Map over all children, invoking a callback with any of them.
+ * Omits children that will not be rendered (null, undefined, false). Flattens
+ * children inside fragments
+ * @param {React.Children} children - Children object as provided by react
+ * @param {Function} fn - Callback that will be called with every child
+ * @param {Boolean} flatten - Whether or not to flatten children inside
+ * fragments. Defaults to true
+ * @return {Array} An array with the results of calling fn with every child in
+ * children
+ */
+const mapChildren = (children, fn, flatten = true) =>
+  React.Children.map(childrenToArray(children, flatten), fn)
+
+module.exports = { countChildren, mapChildren }
